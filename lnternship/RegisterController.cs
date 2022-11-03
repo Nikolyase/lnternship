505dfa63fa743df26f57cc78678ce3aecebe392a
Nikolyase <96522347+Nikolyase@users.noreply.github.com>
2022-11-04 02:07:24 +0300

    Добавьте файлы проекта.

---
+++ lnternship/RegisterController.cs
@@ -0,0 +1,51 @@
+﻿using Internship.ViewModels.Account;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Internship
+{
+    public class RegisterController : Controller
+    {
+        [Route("Register")]
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View("Home/Register");
+        }
+
+        [Route("RegisterPart2")]
+        [HttpGet]
+        public IActionResult RegisterPart2(RegisterViewModel model)
+        {
+            return View("RegisterPart2", model);
+        }
+
+        [Route("Register")]
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            //if (ModelState.IsValid)
+            //{
+            //    var user = _mapper.Map<User>(model);
+
+            //    var result = await _userManager.CreateAsync(user, model.PasswordReg);
+            //    if (result.Succeeded)
+            //    {
+            //        await _signInManager.SignInAsync(user, false);
+            //        return RedirectToAction("Index", "Home");
+            //    }
+            //    else
+            //    {
+            //        foreach (var error in result.Errors)
+            //        {
+            //            ModelState.AddModelError(string.Empty, error.Description);
+            //        }
+            //    }
+            //}
+            return View("RegisterPart2", model);
+        }
+    }
+}
